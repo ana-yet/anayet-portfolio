9dbd8a58f348e639c3b9a93a438c3a5d6562e0ae
ana-yet <anayethusenaj@gmail.com>
2025-10-22 17:11:05 +0600

    fixed the warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
   images: {
     domains: ["images.unsplash.com", "i.pravatar.cc", "images.remotePatterns"],
   },
